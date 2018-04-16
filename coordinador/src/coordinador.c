11183c3c0b00d2a1e2a66874f75fe53d6eceec50
Facundo Ahumada <facundoahumada93@hotmail.com>
2018-04-16 15:36:38 -0300

    Creo el proyecto coordinador

---
+++ coordinador/src/coordinador.c
@@ -0,0 +1,5 @@
+#include "coordinador.h"
+
+int main(void) {
+	return EXIT_SUCCESS;
+}
---
+++ coordinador/src/coordinador.h
@@ -0,0 +1,7 @@
+#ifndef COORDINADOR_H_
+#define COORDINADOR_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif /* COORDINADOR_H_ */
