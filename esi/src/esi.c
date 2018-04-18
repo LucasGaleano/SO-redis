25688755ebd60aa82ca8e166d25ef4403786a41f
Facundo Ahumada <facundoahumada93@hotmail.com>
2018-04-18 20:44:17 -0300

    esi se conecta con el coordinador y con el planiicador

--- esi/src/esi.c
+++ esi/src/esi.c
@@ -3,15 +3,49 @@
 int main(void) {
 
 	//Creo archivo de log
-	t_log * logESI = log_create("log_ESI.log", "esi", true,
-			LOG_LEVEL_TRACE);
+	logESI = log_create("log_ESI.log", "esi", true, LOG_LEVEL_TRACE);
 	log_trace(logESI, "Inicio el proceso esi \n");
+
+	//Conecto esi con planificador y coordinador
+	conectarEsi();
 
 	//Termina esi
 	log_info(logESI, "Termino el proceso esi \n");
 
-	//Destruo archivo de log
+	//Destruyo archivo de log
 	log_destroy(logESI);
 
 	return EXIT_SUCCESS;
 }
+
+/*-------------------------Conexion-------------------------*/
+
+void conectarEsi() {
+	//Leo la configuracion del esi
+	t_config* configEsi = leerConfiguracion();
+
+	//Setteo las variables de configuracion
+	char * coordinadorIP = config_get_string_value(configEsi, "COORDINADOR_IP");
+	int coordinadorPuerto = config_get_int_value(configEsi, "COORDINADOR_PUERTO");
+	char * planificadorIP = config_get_string_value(configEsi, "PLANIFICADOR_IP");
+	int planificadorPuerto = config_get_int_value(configEsi, "PLANIFICADOR_PUERTO");
+
+	//Conecto al coordinador
+	socketCoordinador = conectarCliente(coordinadorIP,coordinadorPuerto,ESI);
+
+	//Conecto al planificador
+	socketPlanificador = conectarCliente(planificadorIP,planificadorPuerto,ESI);
+
+	//Destruyo la configuracion
+	config_destroy(configEsi);
+}
+
+t_config* leerConfiguracion() {
+	t_config* configEsi = config_create(RUTA_CONFIG);
+	if (!configEsi) {
+		log_error(logESI, "Error al abrir la configuracion \n");
+		exit(EXIT_FAILURE);
+	}
+	return configEsi;
+}
+
--- esi/src/esi.h
+++ esi/src/esi.h
@@ -1,9 +1,22 @@
 #ifndef ESI_H_
 #define ESI_H_
 
+#include <biblioteca/sockets.h>
+#include <commons/log.h>
+#include <commons/config.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <commons/log.h>
 
+#define RUTA_CONFIG "/home/utnso/workspace/tp-2018-1c--0/configuraciones/esi.cfg"
+
+/*------------------------Variables globales-------------------------*/
+t_log * logESI;
+
+int socketCoordinador;
+int socketPlanificador;
+
+/*-------------------------Conexion-------------------------*/
+void conectarEsi();
+t_config* leerConfiguracion();
 
 #endif /* ESI_H_ */
