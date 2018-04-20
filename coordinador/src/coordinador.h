977867ca299e4d3a8b52758986c35abeb2b95be8
LucasGaleano <l.a.galeano2@gmail.com>
2018-04-20 16:23:21 -0300

    implemento archivo configuracion

--- coordinador/src/coordinador.h
+++ coordinador/src/coordinador.h
@@ -3,5 +3,30 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <commons/config.h>
+
+#define PATH_CONFIG "/home/utnso/workspace/tp-2018-1c--0/configuraciones/coordinador.cfg"
+
+
+/*---------------------Estructuras-------------------------*/
+
+typedef struct t_configuracion{
+	int puertoConexion;
+	char* algoritmoDist;
+	int cantidadEntradas;
+	int tamanioEntradas;
+	int retardo;
+}t_configuraciones;
+
+
+/*------------------------Globales-------------------------*/
+
+t_configuraciones g_configuracion;
+
+
+/*------------------------FUNCIONES-------------------------*/
+
+t_configuraciones armarConfigCoordinador(t_config*);
+
 
 #endif /* COORDINADOR_H_ */
