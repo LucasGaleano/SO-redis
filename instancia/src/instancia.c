874baec686870877e75ade7d9941bea3599370d7
dichena.federicoC@gmail.com <dichena.federico@gmail.com>
2018-04-20 16:25:46 -0300

    Se sube inicializacion del archivo de configuracion y el path al archivo .cfg

--- instancia/src/instancia.c
+++ instancia/src/instancia.c
@@ -1,5 +1,26 @@
 #include "instancia.h"
 
 int main(void) {
-	return EXIT_SUCCESS;
+
+	instanciaConfig config = cargarConfiguracionInstancia(PATH_CONFIG);
+
+	config_destroy(config.archivoConfig);
+
+	return 0;
 }
+
+instanciaConfig cargarConfiguracionInstancia(char * pathConfig){
+
+	  instanciaConfig result;
+	  result.archivoConfig = config_create(pathConfig);
+	  result.coordinadorIpConfig = config_get_string_value(result.archivoConfig,"COORDINADOR_IP");
+	  result.coordinadorPuertoConfig = config_get_int_value(result.archivoConfig,"COORDINADOR_PUERTO");
+	  result.algoritmoReemplazo = config_get_string_value(result.archivoConfig,"ALGORITMO_REEMPLAZO");
+	  result.puntoMontaje = config_get_string_value(result.archivoConfig,"PUNTO_MONTAJE");
+	  result.nombreInstancia = config_get_string_value(result.archivoConfig,"NOMBRE_INSTANCIA");
+	  result.intervaloDump = config_get_int_value(result.archivoConfig,"INTERVALO_DUMP");
+
+	  return result;
+}
+
+
--- instancia/src/instancia.h
+++ instancia/src/instancia.h
@@ -3,5 +3,27 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <commons/config.h>
+
+// Constante
+
+#define PATH_CONFIG "/home/utnso/workspace/tp-2018-1c--0/configuraciones/instancia.cfg"
+
+// Estructuras
+
+typedef struct instanciaConfig {
+t_config * archivoConfig;
+int coordinadorPuertoConfig;
+char* coordinadorIpConfig;
+char* algoritmoReemplazo;
+char* puntoMontaje;
+char* nombreInstancia;
+int intervaloDump;
+}instanciaConfig;
+
+// Funciones
+
+instanciaConfig cargarConfiguracionInstancia(char* pathConfig);
+
 
 #endif /* INSTANCIA_H_ */
