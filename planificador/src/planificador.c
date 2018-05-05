5cb9ee7980c8e6aa7b4da0254b4d94b0f137a8f6
gastoncastineira <gastoncastineira@hotmail.com>
2018-05-05 19:38:06 -0300

    revision sobre bloqueo por consola

--- planificador/src/planificador.c
+++ planificador/src/planificador.c
@@ -1,85 +1,32 @@
 #include "planificador.h"
 
-double g_est;
+t_dictionary* g_listos;
+t_dictionary* g_exec;
+t_dictionary* g_term;
+t_dictionary* g_bloq;
 
 int main(void) {
 
 	g_listos = dictionary_create();
+	g_exec = dictionary_create();
+	g_term = dictionary_create();
 	g_bloq = dictionary_create();
 
-	g_clavesBloqueadas = list_create();
+	t_config* con = config_create(RUTA_CONFIGURACION_PLANIF);
+	t_log* logger = log_create("", "Planificador", 1, LOG_LEVEL_TRACE);
 
-	g_con = config_create(RUTA_CONFIGURACION_PLANIF);
-	g_logger = log_create("", "Planificador", 1, LOG_LEVEL_TRACE);
-
-	int puertoLocal = config_get_int_value(g_con, "PUERTO");
+	int puertoLocal = config_get_int_value(con, "PUERTO");
 	//hablar tipo del algoritmo de planificacion
-	g_est = config_get_double_value(g_con, "ESTIMACION_INICIAL");
-	char* ip = config_get_string_value(g_con, "COORDINADOR_IP");
-	int puertoCoordinador = config_get_int_value(g_con, "COORDINADOR_PUERTO");
-	asignarBloquedas(config_get_array_value(g_con, "CLAVES_BLOQUEADAS"));
+	double est = config_get_double_value(con, "ESTIMACION_INICIAL");
+	char* ip = config_get_string_value(con, "COORDINADOR_IP");
+	int puertoCoordinador = config_get_int_value(con, "COORDINADOR_PUERTO");
+	//claves bloqueadas
 
-	pthread_t* hiloServer;
-	pthread_t* hiloAlgoritmos;
 
-	pthread_create(hiloServer, NULL, (void*) iniciarServidor, puertoLocal);
-	//iniciar Algoritmos en hilo
 
-	//iniciarConsola
-
-	pthread_join(hiloServer, NULL);
-
-	config_destroy(g_con);
-	log_destroy(g_logger);
+	config_destroy(con);
+	log_destroy(logger);
 
 	return EXIT_SUCCESS;
 }
 
-asignarBloquedas(char** codigos)
-{
-	int i = 0;
-	while(codigos[i] != NULL)
-	{
-		t_infoClavezBloqueadas insert = malloc(sizeof(t_infoClavezBloqueadas));
-		insert->codigo = strdup(codigos[i]);
-		insert->consola = 1;
-		list_add(g_clavesBloqueadas, insert);
-	}
-}
-
-procesarPaquete(t_paquete* unPaquete, int* socketCliente)
-{
-	t_infoListos *dat;
-	switch(unPaquete->codigoOperacion)
-	{
-	case HANDSHAKE:
-		recibirHandshakePlanif(unPaquete, socketCliente);
-		break;
-	case ENVIAR_IDENTIFICACION:
-		dat = malloc(sizeof(t_infoListos));
-		dat->estAnterior = g_est;
-		dat->realAnterior = g_est;
-		dat->socketESI = *socketCliente;
-		dat->tEnEspera = 0;
-		dictionary_put(g_listos, recibirIdentificacion(unPaquete), dat);
-	}
-	destruirPaquete(unPaquete);
-}
-
-recibirHandshakePlanif(t_paquete* unPaquete,int* socketCliente)
-{
-	int tipoCliente;
-	memcpy(&tipoCliente, unPaquete->buffer->data, sizeof(int));
-	switch(tipoCliente)
-	{
-	case ESI:
-		break;
-	default:
-		*socketCliente = -1;
-	}
-}
-
-void iniciarServidor(int puerto)
-{
-	iniciarServer(puerto, (void*)procesarPaquete, g_logger);
-}
--- planificador/src/planificador.h
+++ planificador/src/planificador.h
@@ -6,46 +6,8 @@
 #include <commons/config.h>
 #include <commons/log.h>
 #include <commons/collections/dictionary.h>
-#include <pthread.h>
-#include <biblioteca/sockets.h>
-#include <biblioteca/paquetes.h>
-#include <biblioteca/estructuras.h>
 
 #define RUTA_CONFIGURACION_PLANIF "home/utnso/workspace/configuraciones/planificador.cfg"
 
-
-typedef struct
-{
-	int socketESI, tEnEspera;
-	double estAnterior, realAnterior;
-}t_infoListos;
-
-
-typedef struct
-{
-	char* codRecurso;
-	t_infoListos rafagasYsocket;
-}t_infoBloqueo;
-
-typedef struct
-{
-	char* codigo;
-	int consola;
-}t_infoClavezBloqueadas;
-
-pthread_mutex_t mutexConsola = PTHREAD_MUTEX_INITIALIZER;
-
-t_dictionary* g_listos;
-t_dictionary* g_bloq;
-
-t_list* g_clavesBloqueadas;
-
-t_log* g_logger;
-t_config* g_con;
-
-void procesarPaquete(t_paquete*, int*);
-void recibirHanshakePlanif(t_paquete*, int*);
-void iniciarServidor(int);
-void asignarBloquedas(char**);
 
 #endif /* PLANIFICADOR_H_ */
