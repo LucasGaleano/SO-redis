8cf68d0815ac99a44063b1e9a3dce31e774a21fb
lucasgaleano <l.a.galeano2@gmail.com>
2018-05-30 16:57:29 -0300

    agrego carpeta para pruebas

---
+++ coordinador/prueba/prueba.h
@@ -0,0 +1,17 @@
+#ifndef PRUEBA_H_
+#define PRUEBA_H_
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <commons/config.h>
+#include <biblioteca/sockets.h>
+#include <biblioteca/paquetes.h>
+#include <biblioteca/estructuras.h>
+#include "../src/tablasAdministrativas.h"
+
+t_list * PruebaInstacias(t_list* tablaDeInstancias);
+
+
+
+#endif
